59747aade55ada12a6f9718b1c3399a9d1cb7cdb
Wajud <wajudkareem757@gmail.com>
2024-08-31 13:15:19 +0100

    updated port in server and http url in client

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const nodemailer = require("nodemailer");
 
 const app = express();
 
-const PORT = 5500;
+const PORT = process.env.PORT || 5500;
 
 app.use(express.json({ limit: "25mb" }));
 app.use(express.urlencoded({ limit: "25mb" }));
